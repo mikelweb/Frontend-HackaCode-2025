48e9b3064777e0e47e74beec8decc8875766d77a
mikelweb <info@mikelweb.net>
2025-02-07 08:16:46 +0100

    Doctor add and edit components

---
+++ src/app/doctor/doctor-add/doctor-add.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-add',
+  imports: [],
+  templateUrl: './doctor-add.component.html',
+  styleUrl: './doctor-add.component.scss'
+})
+export class DoctorAddComponent {
+
+}
---
+++ src/app/doctor/doctor-edit/doctor-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-edit',
+  imports: [],
+  templateUrl: './doctor-edit.component.html',
+  styleUrl: './doctor-edit.component.scss'
+})
+export class DoctorEditComponent {
+
+}
