15cb7db07dc73b3bc619021d2d52721e0d160ea1
Mikel Goyeneche <info@mikelweb.net>
2025-02-15 23:49:02 +0100

    Patients not persons

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -12,24 +12,24 @@ export class PatientService {
 
   public getPatients() : Observable<PatientModel[]> {
     // let url : string = '/assets/mock/patients.json';
-    let url : string = 'http://localhost:8080/api/v1/persons';
+    let url : string = 'http://localhost:8080/api/v1/patients';
 
     return this.http.get<PatientModel[]>(url);
   }
 
   public getPatient(patientId : number) : Observable<PatientModel> {
     // let url : string = '/assets/mock/patient-' + patientId + '.json';
-    let url : string = 'http://localhost:8080/api/v1/persons/' + patientId;
+    let url : string = 'http://localhost:8080/api/v1/patients/' + patientId;
     return this.http.get<PatientModel>(url);
   }
 
   public updatePatient(patient : PatientModel) : Observable<boolean> {
-    let url : string = 'http://localhost:8080/api/v1/persons/' + patient.id_paciente;
+    let url : string = 'http://localhost:8080/api/v1/patients/' + patient.id_paciente;
     return this.http.put<boolean>(url, patient);
   }
 
   public deletePatient(patientId : number) : Observable<boolean> {
-    let url : string = 'http://localhost:8080/api/v1/persons/' + patientId;
+    let url : string = 'http://localhost:8080/api/v1/patients/' + patientId;
     return this.http.delete<boolean>(url);
   }
 }
