5757959378f0b9dc55cfdd85b2d39ea3fecf143d
Mikel Goyeneche <info@mikelweb.net>
2025-02-18 11:55:56 +0100

    Add medical service functionality

--- src/app/medical-service/medical-service-add/medical-service-add.component.ts
+++ src/app/medical-service/medical-service-add/medical-service-add.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
+import { MedicalServicesService } from '../../services/medical-services.service';
+import { MedicalServiceModel } from '../../model/medical-service.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-medical-service-add',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './medical-service-add.component.html',
   styleUrl: './medical-service-add.component.scss'
 })
 export class MedicalServiceAddComponent {
 
+  public nombre : string = '';
+  public descripcion : string = '';
+  public precio : number = 0;
+
+  
+  constructor(private medicalServiceService : MedicalServicesService) { }
+
+  public createMedicalService() : void {
+      // Call service
+      let medicalService : MedicalServiceModel = {
+          codigo_servicio : 0,
+          nombre : this.nombre,
+          descripcion : this.descripcion,
+          precio : this.precio
+      };
+      this.medicalServiceService.createMedicalService(medicalService).subscribe(response => {
+        console.log(response);
+      });
+  }
 }
--- src/app/services/medical-services.service.ts
+++ src/app/services/medical-services.service.ts
@@ -27,6 +27,11 @@ export class MedicalServicesService {
     return this.http.put<boolean>(url, medicalService);
   }
 
+  public createMedicalService(medicalService : MedicalServiceModel) : Observable<boolean> {
+    let url : string = 'http://localhost:8080/api/v1/medicalservices';
+    return this.http.post<boolean>(url, medicalService);
+  }
+
   public deleteMedicalService(medicalServiceId : number) : Observable<boolean> {
     let url : string = 'http://localhost:8080/api/v1/medicalservices/' + medicalServiceId;
     return this.http.delete<boolean>(url);
